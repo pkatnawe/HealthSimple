3e3aaeff6176b1f44b49975cd0d89194a67a41c8
June Oh <juneoh@dyn172-30-73-227.wireless.uwo.pri>
2022-11-19 14:42:58 -0500

    add landing page styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+import landing from './landing.png';
 import './App.css';
+import Topbar from './Components/Topbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" background={landing} style={{
+        backgroundImage: `url(${landing})`,
+        backgroundSize: 'cover',
+        overflow: 'hidden'
+      }}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Topbar></Topbar>
+        <h1>Do health right</h1>
+        <h3>Search your ingredients and such and get your health info.</h3>
+        <a className="btn" href="">Get started</a>
       </header>
     </div>
   );
---
+++ frontend/src/Components/Topbar.js
@@ -0,0 +1,10 @@
+import './Topbar.css'
+
+export default function Topbar() {
+    return (
+      <div className="topbar">
+        <h2 className="logo">Healthsimple</h2>
+        <a className="top-btn" href="">Get started</a>
+      </div>
+    );
+  }
